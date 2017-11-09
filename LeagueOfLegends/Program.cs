456b6c298c54f9350c004104a9e801ad33cacbdc
Selcuk Ozturk <selcukahmed@gmail.com>
2017-11-09 23:54:13 +0300

    Init

---
+++ LeagueOfLegends/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeagueOfLegends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
